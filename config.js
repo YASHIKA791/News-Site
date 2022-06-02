305fd54eee9f020a5e0b79838173662f1d442784
Yashika <mails.yashika@gmail.com>
2022-06-02 20:41:54 +0530

    config file added

---
+++ config.js
@@ -0,0 +1,6 @@
+var config={
+    NEWS_API:"https://gnews.io/api/v4/search?q=india&token=658b157cdebda978507d1fddf5f6d078&lang=en",
+    EVENT_API:"85d6214687msh75dc5f4e5b8cc39p116bd4jsn42222cb736d8",
+    TEMP_API:"e187219afeb3287cbed4dfe74f29168e",
+    NEWS_TOKEN:"658b157cdebda978507d1fddf5f6d078"
+}
